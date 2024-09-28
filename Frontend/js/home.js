7b7710eb943e0fafbd9439150935c14aa18e4a43
Flavia <flavia_gudy@hotmail.com>
2024-09-28 01:46:43 -0300

    Adicionado página de API Key e Partida

--- Frontend/js/home.js
+++ Frontend/js/home.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const openModalButton = document.getElementById('open-modal');
     const closeModalButton = document.getElementById('close-modal');
-    const partidaPesquisada = document.querySelector('.partida-pesquisada');
+    const cardAviso = document.querySelector('.card-aviso');
 
     // Ao abrir, esconde o botão de abrir e mostra o de fechar e a div
     openModalButton.addEventListener('click', () => {
-        partidaPesquisada.classList.remove('hide');
+        cardAviso.classList.remove('hide');
         openModalButton.classList.add('hide');
         closeModalButton.classList.remove('hide');
     });
 
     // Ao fechar, mostra o botão de abrir e esconde o de fechar e a div
     closeModalButton.addEventListener('click', () => {
-        partidaPesquisada.classList.add('hide');
+        cardAviso.classList.add('hide');
         openModalButton.classList.remove('hide');
         closeModalButton.classList.add('hide');
     });
